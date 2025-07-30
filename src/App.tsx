b30d04b00e23fe9feb833bb211f186d3b34f7c25
Mahadevan Reji <rmahadevan574@gmail.com>
2025-07-30 20:42:23 +0530

    Refactor App component to preload images on mount

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import './App.css'
-import HeroSection from './components/Hero/Hero'
+import { useEffect } from "react";
+import "./App.css";
+import HeroSection from "./components/Hero/Hero";
 
+const lexImg = "/chars/lex.png";
+const sparkImg = "/chars/spark.png";
+const syntaxImg = "/chars/syntax.png";
+const monitor = "/monitor.png"; 
 
 
 function App() {
+  useEffect(() => {
+    //preload images
+    const images = [lexImg, sparkImg, syntaxImg, monitor];
+    images.forEach((image) => {
+      const img = new Image();
+      img.src = image;
+      img.onload = () => {
+        // Image preloaded successfully
+      };
+    });
+  }, []);
   return (
     <>
-     <HeroSection />
+      <HeroSection />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
